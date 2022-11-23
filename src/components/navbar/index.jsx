41999167197cd7d2663cd4d3511891c6a2728838
Raiee Gulhane <64693025+raieegulhane@users.noreply.github.com>
2022-11-24 02:38:06 +0530

    navbar-style-updated

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -2,8 +2,11 @@ import "./navbar.css";
 
 const Navbar = () => {
     return(
-        <nav className="navbar-wrapper flex flex-row flex-justify-sb">
-            <h1 className="navbar-heading">React App</h1>
+        <nav className="navbar-wrapper flex flex-row flex-justify-sb flex-align-cn">
+            <div className="navbar-logo flex">
+                <i className="fa-brands fa-react"></i>
+                <span>React App</span>
+            </div>
             <button className="button-logout btn-secondary">Logout</button>
         </nav>
     );
